a2fdd122b5449162bfbf608c8e68fed4a6c003d5
laure-delisle <laure.delisle@gmail.com>
2016-04-03 03:30:34 +0200

    Controler draft

--- src/main/java/fr/esiea/tetris/controler/Controler.java
+++ src/main/java/fr/esiea/tetris/controler/Controler.java
@@ -1,5 +1,14 @@
 package fr.esiea.tetris.controler;
 
-public class Controler {
+import fr.esiea.tetris.model.*;
+import fr.esiea.tetris.view.*;
 
+public class Controler {
+	Model model;
+	View view;
+	
+	public Controler() {
+		model = new Model();
+		view = new View();
+	}
 }
