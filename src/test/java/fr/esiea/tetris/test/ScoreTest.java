c8b09e16b99d5b89eb3e5339f49b785b6cc31b3b
laure-delisle <laure.delisle@gmail.com>
2016-04-03 02:36:21 +0200

    Test

---
+++ src/test/java/fr/esiea/tetris/test/ScoreTest.java
@@ -0,0 +1,23 @@
+package fr.esiea.tetris.test;
+
+import fr.esiea.tetris.model.*;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class ScoreTest {
+	@Test
+	public void shouldCreateScore() {
+		Score score = new Score();
+		assertEquals(score.getScore(),0);
+	}
+	
+	@Test
+	public void shouldIncrementScore() {
+		Score score = new Score();
+		score.incrementScore();
+		assertEquals(score.getScore(),10);
+	}
+	
+	
+}
