38ef081f5e08fa7d6788e3fd3d965beab4c232de
laure-delisle <laure.delisle@gmail.com>
2016-04-03 03:39:28 +0200

    drafting board

--- src/main/java/fr/esiea/tetris/model/Board.java
+++ src/main/java/fr/esiea/tetris/model/Board.java
@@ -1,5 +1,20 @@
 package fr.esiea.tetris.model;
 
 public class Board {
-
+	int[][] boardArray;
+	
+	public Board() {
+		boardArray = new int[20][10];
+	}
+	
+	public void checkLine() {
+		//TODO
+	}
+	
+	public int cleanLines(){
+		//TODO
+		int nboflinescleaned=0;
+		return (nboflinescleaned);
+	}
+	
 }
--- src/main/java/fr/esiea/tetris/model/Model.java
+++ src/main/java/fr/esiea/tetris/model/Model.java
@@ -1,5 +1,23 @@
 package fr.esiea.tetris.model;
 
 public class Model {
-
+	Score score;
+	Board board;
+	Tetrimino tetrimino;
+	int tetX;
+	int tetY;
+	
+	public Model() {
+		score = new Score();
+		board = new Board();
+		tetrimino = new Tetrimino();
+	}
+	
+	public void newTetrimino()
+	{
+		tetrimino.init();
+		tetX = 0;
+		tetY = 3;
+	}
+	
 }
